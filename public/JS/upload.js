03d3f34cbcc54d97e06dd591d56b5e5bd4399950
Kilopa Nye <steve69988@gmail.com>
2023-11-12 21:31:27 +0800

    update upload.js's img onload function

--- public/JS/upload.js
+++ public/JS/upload.js
@@ -2,26 +2,23 @@
 
 
 function previewImage() {
-    var fileInput = document.querySelector('.img-upload');
-    var imagePreview = document.querySelector('.img-preview');
+    let fileInput = document.querySelector('.img-upload');
+    let imagePreview = document.querySelector('.img-preview');
 
-    // 确保选择了文件
     if (fileInput.files && fileInput.files[0]) {
-        var reader = new FileReader();
+        let reader = new FileReader();
 
-        for (var i = 0; i < fileInput.files.length; i++) {
-            var reader = new FileReader();
+        for (let i = 0; i < fileInput.files.length; i++) {
+            let reader = new FileReader();
 
             reader.onload = function(e) {
-                // 创建一个新的图像元素
-                var img = document.createElement('img');
+
+                let img = document.createElement('img');
                 img.src = e.target.result;
 
-                // 将预览图添加到页面
                 imagePreview.appendChild(img);
             };
 
-            // 读取文件内容并触发onload事件
             reader.readAsDataURL(fileInput.files[i]);
     }
 }}
