363f481ec2f83984198ab1bae0dae900c34bfb23
Kilopa Nye <steve69988@gmail.com>
2024-01-09 23:02:10 +0800

    restyle:set product tag style-margin-bottom.

--- model/query_make.py
+++ model/query_make.py
@@ -413,7 +413,7 @@ def get_pre_trade_info(seller_id):
         cursor = con.cursor(dictionary=True)
 
         cursor.execute(
-            "SELECT  members.username, img.image_url, pre.*, a.owner_pre_site, a.id, a.product_name, a.product_price, a.product_amount FROM pre_order_info as pre JOIN product_info as a ON pre.product_id = a.id JOIN product_images as img ON img.product_id = a.id JOIN members ON members.id = a.owner_id  WHERE pre.seller_id = %s GROUP BY a.id",
+            "SELECT  members.username, img.image_url, pre.*, a.owner_pre_site, a.id, a.product_name, a.product_price, a.product_amount FROM pre_order_info as pre JOIN product_info as a ON pre.product_id = a.id JOIN product_images as img ON img.product_id = a.id JOIN members ON members.id = a.owner_id  WHERE pre.seller_id = %s GROUP BY pre.id",
             (seller_id,),
         )
         response = cursor.fetchall()
